435213d77613adf0af3d0f044890931efcfecaba
cmmanik <mkmanik6523u@gmial.com>
2019-02-14 21:26:35 +0600

    Updates

---
+++ precache-manifest.b5c652f499d1966a001e4cb885d05948.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b6f225bb9dac2e07552",
+    "url": "/myportfolio/static/js/runtime~main.6b6f225b.js"
+  },
+  {
+    "revision": "8c4a37debd8250c4a129",
+    "url": "/myportfolio/static/js/main.8c4a37de.chunk.js"
+  },
+  {
+    "revision": "7fd576d16077cf9ce3b3",
+    "url": "/myportfolio/static/js/1.7fd576d1.chunk.js"
+  },
+  {
+    "revision": "8c4a37debd8250c4a129",
+    "url": "/myportfolio/static/css/main.425cfe50.chunk.css"
+  },
+  {
+    "revision": "7fd576d16077cf9ce3b3",
+    "url": "/myportfolio/static/css/1.11125ffd.chunk.css"
+  },
+  {
+    "revision": "5943ab661ce2b8bd56a2ebd77cf67d92",
+    "url": "/myportfolio/index.html"
+  }
+];
